4e57adf1fd0ad2e2b2cb5ac48c204dd4b2c4a47e
Michael McClellan <werothegreat@yahoo.com>
2013-04-02 16:13:40 -0400

    mhmm

--- card.py
+++ card.py
@@ -5,18 +5,37 @@ PROJECTILE = 0x0002
 EFFECT = 0x0004
 CREATURE = 0x0008
 
+FROST = 'frost'
+FIRE = 'fire'
+GLASS = 'glass'
+SILK = 'silk'
+METAL = 'metal'
+LIGHTNING = 'lightning'
+EARTH = 'earth'
+POISON = 'poison'
+ILLUSION = 'illusion'
+BLOOD = 'blood'
+
 class Card(object):
 
     #Represents a card
 
-    def put_out(self):
+    def __init__(self):
 
         pass
-
-    def use(self):
-
+        
+    def put_out(self, game, player):
+        #Defines what happens when card is put into play
         pass
 
-    def when_remove(self):
-
+    def activate(self, game, player):
+        #Defines what happens when the card is activated during player's turn
+        #or another player's turn
         pass
+
+    def when_remove_to_hand(self, game, player):
+        #Defines what happens when the card is removed from play to hand
+        pass
+
+    def when_remove_to_discard(self, game, player):
+        #Defines what happens when the card is removed from play to discard
---
+++ player.py
@@ -0,0 +1,21 @@
+from deck import Deck
+
+class Player(object):
+
+    #Represents player who plays game
+    def __init__(self, name, game):
+        self.name = name
+        self.game = game
+        self.draw = Deck()
+        self.hand = Deck()
+        self.inPlay = Deck()
+        self.discard = Deck()
+        self.focusTotal = {}
+        self.usedFocus = {}
+
+    def move_card_to(self, card, target_deck):
+        #Moves card to/from deck, hand, play, discard
+        for deck in (self.draw, self.hand, self.inPlay, self.discard):
+            if card in deck:
+                deck.remove(card)
+        target_deck.add(card)
